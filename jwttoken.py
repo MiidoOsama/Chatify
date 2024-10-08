6dfbb2aa7055a718c2afb4bd30333f27686036c6
MiidoOsama <xmiido@yandex.com>
2024-10-08 12:14:31 +0300

    more secure

--- jwttoken.py
+++ jwttoken.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # from main import TokenData
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
+SECRET_KEY = os.environ['SECRET_KEY']
+ALGORITHM = os.environ['ALGORITHM']
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
